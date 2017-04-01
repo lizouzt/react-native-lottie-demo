17db81e969587a6e344ff2657599f7e3f1721abc
Tao.z <zhangtao@codoon.com>
2017-04-01 18:37:03 +0800

    udpate

--- index.ios.js
+++ index.ios.js
@@ -14,30 +14,57 @@ import {
   Animated
 } from 'react-native';
 
+const makeExample = (name, getJson) => ({ name, getJson });
+const EXAMPLES = [
+  makeExample('Hamburger Arrow', () => require('./animations/HamburgerArrow.json')),
+  makeExample('Line Animation', () => require('./animations/LineAnimation.json')),
+  makeExample('Lottie Logo 1', () => require('./animations/LottieLogo1.json')),
+  makeExample('Lottie Logo 2', () => require('./animations/LottieLogo2.json')),
+  makeExample('Lottie Walkthrough', () => require('./animations/LottieWalkthrough.json')),
+  makeExample('Pin Jump', () => require('./animations/PinJump.json')),
+  makeExample('Twitter Heart', () => require('./animations/TwitterHeart.json')),
+  makeExample('Watermelon', () => require('./animations/Watermelon.json')),
+  makeExample('Motion Corpse', () => require('./animations/MotionCorpse-Jrcanest.json')),
+].reduce((acc, e) => {
+  // eslint-disable-next-line no-param-reassign
+  acc[e.name] = e;
+  return acc;
+}, {});
+
 export default class rnAnimateTest extends Component {
   constructor (props) {
     super(props);
     this.state = {
       progress: new Animated.Value(0),
+      config: {
+        duration: 3000,
+        imperative: false,
+      },
     };
   }
 
-  componentDidMount () {
+  runAnimate () {
     Animated.timing(this.state.progress, {
       toValue: 1,
-      duration: 2000
+      duration: this.state.config.duration
     }).start();
+  }
+
+  componentDidMount () {
+    this.runAnimate.bind(this)();
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Animation style={{
-          width: 200,
-          height: 200
-        }}
-        source={require("./animations/LineAnimation.json")}>
-        </Animation>
+            width: 100,
+            height: 100
+          }}
+          source={EXAMPLES['Hamburger Arrow'].getJson()}
+          progress={this.state.progress}
+          onclick={() => {this.setState({progress: new Animated.Value(0)}); this.runAnimate.bind(this)()}}
+        />
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
